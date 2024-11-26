ecc9551d80fda7640221b66baa5a8869c5165364
MafiieuHell <mehdikerkar@outlook.fr>
2024-11-26 11:13:12 +0100

    filter games by platform 'playstation don't work'

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,5 +1,5 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
-import useGames from "../hooks/useGames";
+import useGames, { Platform } from "../hooks/useGames";
 import { Genre } from "../hooks/useGenre";
 import GameCard from "./GameCard";
 import GameCardContainair from "./GameCardContainair";
@@ -7,10 +7,11 @@ import GameCardSkeleton from "./GameCardSkeleton";
 
 interface Props {
   selectedGenre: Genre | null;
+  selectedPlatfrom: Platform | null;
 }
 
-const GameGrid = ({ selectedGenre }: Props) => {
-  const { data, error, isLoading } = useGames(selectedGenre);
+const GameGrid = ({ selectedGenre, selectedPlatfrom }: Props) => {
+  const { data, error, isLoading } = useGames(selectedGenre, selectedPlatfrom);
   const skeletons = [1, 2, 3, 4, 5, 6];
 
   return (
